588d1e3a0c6baf20af2decc76d1cfc81be2987df
levk02011 <103530435+levk02011@users.noreply.github.com>
2024-05-27 15:48:08 +0300

    Add files via upload

---
+++ second_file.js
@@ -0,0 +1,13 @@
+const {OAuth2Client} = require('google-auth-library');
+const client = new OAuth2Client(CLIENT_ID);
+
+async function verify(token) {
+  const ticket = await client.verifyIdToken({
+      idToken: token,
+      audience: CLIENT_ID,  // Specify the CLIENT_ID of the app that accesses the backend
+  });
+  const payload = ticket.getPayload();
+  const userid = payload['sub'];
+  // Отримайте інформацію про користувача з payload і зробіть потрібні дії
+}
+verify().catch(console.error);
